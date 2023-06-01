9399128f4418d64b960baa02c0a5aa72733c8606
Sarabik <dalilija@inbox.lv>
2023-06-01 19:14:58 +0300

    5. Список кандидатов [#504842  [#504842]]

---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,58 @@
+package ru.job4j.dreamjob.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class MemoryCandidateRepository implements CandidateRepository {
+
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0, "Anton Shilov", "Intern Java Developer", LocalDateTime.of(2023, 2, 10, 12, 30)));
+        save(new Candidate(0, "Lisa Rebova", "Middle Java Developer", LocalDateTime.of(2023, 4, 2, 8, 10)));
+        save(new Candidate(0, "Ivan Lenov", "Senior Java Developer", LocalDateTime.of(2023, 5, 29, 9, 44)));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return candidates.remove(id) != null;
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(), (id, oldVacancy) ->
+                new Candidate(oldVacancy.getId(), candidate.getName(), candidate.getDescription(), candidate.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
