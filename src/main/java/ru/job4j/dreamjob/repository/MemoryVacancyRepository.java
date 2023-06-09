2c1c7772c1f54128ec32594f3f99fa8590447e99
Sarabik <dalilija@inbox.lv>
2023-06-09 11:41:50 +0300

    1. Формы. Поля ввода. [#504853]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -20,12 +20,12 @@ public class MemoryVacancyRepository implements VacancyRepository {
     private final Map<Integer, Vacancy> vacancies = new ConcurrentHashMap<>();
 
     private MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer", "description1", LocalDateTime.of(2023, 1, 10, 12, 30)));
-        save(new Vacancy(0, "Junior Java Developer", "description2", LocalDateTime.of(2023, 2, 11, 12, 38)));
-        save(new Vacancy(0, "Junior+ Java Developer", "description3", LocalDateTime.of(2023, 4, 3, 15, 30)));
-        save(new Vacancy(0, "Middle Java Developer", "description4", LocalDateTime.of(2023, 5, 1, 12, 36)));
-        save(new Vacancy(0, "Middle+ Java Developer", "description5", LocalDateTime.of(2023, 5, 10, 10, 10)));
-        save(new Vacancy(0, "Senior Java Developer", "description6", LocalDateTime.of(2023, 5, 22, 17, 20)));
+        save(new Vacancy(0, "Intern Java Developer", "description1", LocalDateTime.of(2023, 1, 10, 12, 30), true));
+        save(new Vacancy(0, "Junior Java Developer", "description2", LocalDateTime.of(2023, 2, 11, 12, 38), true));
+        save(new Vacancy(0, "Junior+ Java Developer", "description3", LocalDateTime.of(2023, 4, 3, 15, 30), true));
+        save(new Vacancy(0, "Middle Java Developer", "description4", LocalDateTime.of(2023, 5, 1, 12, 36), true));
+        save(new Vacancy(0, "Middle+ Java Developer", "description5", LocalDateTime.of(2023, 5, 10, 10, 10), true));
+        save(new Vacancy(0, "Senior Java Developer", "description6", LocalDateTime.of(2023, 5, 22, 17, 20), true));
     }
 
     @Override
@@ -43,7 +43,8 @@ public class MemoryVacancyRepository implements VacancyRepository {
     @Override
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) ->
-                new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), vacancy.getCreationDate())) != null;
+                new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(),
+                        vacancy.getCreationDate(), vacancy.getVisible())) != null;
     }
 
     @Override
